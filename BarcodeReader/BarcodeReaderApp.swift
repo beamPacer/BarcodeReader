4613cc3ff5ba3d4f4897b518dbe0af282c3f3956
Emma Sinclair <beampacer@gmail.com>
2023-07-22 16:32:58 -0700

    Initial Commit

---
+++ BarcodeReader/BarcodeReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarcodeReaderApp.swift
+//  BarcodeReader
+//
+//  Created by Emma Sinclair on 7/22/23.
+//
+
+import SwiftUI
+
+@main
+struct BarcodeReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BarcodeReader/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  BarcodeReader
+//
+//  Created by Emma Sinclair on 7/22/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
