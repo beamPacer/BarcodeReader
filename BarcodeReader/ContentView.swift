b916b04ce2d3731abd10316ba233f7b6933dba9f
Emma Sinclair <beampacer@gmail.com>
2023-07-22 18:01:35 -0700

    Improves layout

--- BarcodeReader/ContentView.swift
+++ BarcodeReader/ContentView.swift
@@ -14,73 +14,80 @@ struct ContentView: View {
 	@State private var barcodeContent: [String] = []
 
 	var body: some View {
-		Spacer()
-		
-		VStack {
-			if let image = imageLoader.image {
-				Image(nsImage: image)
-					.resizable()
-					.aspectRatio(contentMode: .fit)
-				if barcodeContent.isEmpty {
-					Text("No results")
-						.foregroundColor(.red)
-				} else {
-					ForEach(barcodeContent, id: \.self) { content in
-						HStack {
-							if
-								let url = URL(string: content),
-								let scheme = url.scheme,
-								!scheme.isEmpty
-							{
-								Link(content, destination: url)
-									.foregroundColor(.blue)
-									.underline()
-							} else {
-								Text(content)
+		GeometryReader { geometry in
+			VStack {
+				VStack {
+					if let image = imageLoader.image {
+						Image(nsImage: image)
+							.resizable()
+							.aspectRatio(contentMode: .fit)
+						if barcodeContent.isEmpty {
+							Text("No results")
+								.foregroundColor(.red)
+						} else {
+							ForEach(barcodeContent, id: \.self) { content in
+								HStack {
+									if
+										let url = URL(string: content),
+										let scheme = url.scheme,
+										!scheme.isEmpty
+									{
+										Link(content, destination: url)
+											.foregroundColor(.blue)
+											.underline()
+									} else {
+										Text(content)
+									}
+									Button(action: {
+										let pasteboard = NSPasteboard.general
+										pasteboard.declareTypes([.string], owner: nil)
+										pasteboard.setString(content, forType: .string)
+									}) {
+										Text("Copy")
+									}
+								}
 							}
-							Button(action: {
-								let pasteboard = NSPasteboard.general
-								pasteboard.declareTypes([.string], owner: nil)
-								pasteboard.setString(content, forType: .string)
-							}) {
-								Text("Copy")
+						}
+					} else {
+						Text("No image selected.")
+					}
+				}
+				.onChange(of: imageLoader.image) { newValue in
+					if let newImage = newValue {
+						barcodeReader.readBarcode(from: newImage) { result in
+							switch result {
+							case .failure(let error):
+								print(error)
+							case .success(let result):
+								self.barcodeContent = result
 							}
 						}
 					}
 				}
-			} else {
-				Text("No image selected.")
-			}
-			
-			Button(action: {
-				let panel = NSOpenPanel()
-				panel.canChooseFiles = true
-				panel.canChooseDirectories = false
-				panel.allowsMultipleSelection = false
-				panel.allowedContentTypes = [.png, .jpeg, .tiff]
-				panel.begin { response in
-					if response == .OK, let url = panel.url {
-						self.imageLoader.load(from: url)
+				.padding([.top, .horizontal])
+				
+				Spacer()
+
+				Button(action: {
+					let panel = NSOpenPanel()
+					panel.canChooseFiles = true
+					panel.canChooseDirectories = false
+					panel.allowsMultipleSelection = false
+					panel.allowedContentTypes = [.png, .jpeg, .tiff]
+					panel.begin { response in
+						if response == .OK, let url = panel.url {
+							self.imageLoader.load(from: url)
+						}
 					}
+				}) {
+					Text("Load Image")
+						.padding()
+						.frame(minWidth: 0, maxWidth: .infinity)
+						.cornerRadius(10)
 				}
-			}) {
-				Text("Load Image")
+				.padding()
 			}
 		}
-		.onChange(of: imageLoader.image) { newValue in
-			if let newImage = newValue {
-				barcodeReader.readBarcode(from: newImage) { result in
-					switch result {
-					case .failure(let error):
-						print(error)
-					case .success(let result):
-						self.barcodeContent = result
-					}
-				}
-			}
-		}
-		
-		Spacer()
 	}
 }
 
